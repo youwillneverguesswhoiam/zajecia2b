1ac2f27fed105e85aac0167d1d792e2a8d60c3e7
youwillneverguesswhoiam <j.mikulska2@student.uw.edu.pl>
2017-11-01 20:35:25 +0100

    Zajecia2

---
+++ src/sample/Controller.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+
+    public TextField textfield;
+    public Label label;
+
+    public void handleClick(ActionEvent actionEvent) {
+        textfield.setText("Kliknięto guzik");
+    }
+}
